14d6740ee4fe1b4d6c4a2b8f93c0babea8bd0803
Hikmat Dhamee <me.hemant.available@gmail.com>
2014-04-04 03:07:25 -0700

    no need to specify main class

--- runner.sh
+++ runner.sh
@@ -1,1 +1,1 @@
-$HADOOP_HOME/bin/hadoop jar target/BigSchoolMapReduce-1.0.jar com.bigschool.driver.App input/input.txt output/output.txt
+$HADOOP_HOME/bin/hadoop jar target/BigSchoolMapReduce-1.0.jar input/input.txt output/output.txt
