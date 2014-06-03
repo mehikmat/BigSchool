5458d88c1243504df3a34611bdffe28fd41dc39c
Hikmat Dhamee <me.hemant.available@gmail.com>
2014-06-03 11:47:54 +0545

    add cascading app

--- src/main/java/com/bigschool/Main.java
+++ src/main/java/com/bigschool/Main.java
@@ -2,6 +2,9 @@ package com.bigschool;
 
 import cascading.flow.Flow;
 import cascading.flow.hadoop.HadoopFlowConnector;
+import cascading.operation.aggregator.Count;
+import cascading.pipe.Every;
+import cascading.pipe.GroupBy;
 import cascading.pipe.Pipe;
 import cascading.scheme.hadoop.TextDelimited;
 import cascading.tap.SinkMode;
@@ -14,20 +17,24 @@ import cascading.tuple.Fields;
  */
 public class Main {
 
+    public void run() {
+        Tap src = new Hfs(new TextDelimited(new Fields("line")), "input/input.txt", SinkMode.KEEP);
+        Tap snk = new Hfs(new TextDelimited(new Fields("line")), "output", SinkMode.REPLACE);
+
+        Pipe pipe = new Pipe("copy");
+        pipe=new GroupBy(pipe,new Fields("line"));
+        pipe=new Every(pipe,new Count(new Fields("line")),Fields.RESULTS);
+
+        Flow flow = new HadoopFlowConnector().connect(src, snk, pipe);
+
+        flow.complete();
+
+    }
+
     public static void main(String[] args) {
         new Main().run();
     }
-   public void run() {
-       Tap src = new Hfs(new TextDelimited(new Fields("line")), "input/input.txt", SinkMode.KEEP);
-       Tap snk = new Hfs(new TextDelimited(new Fields("line")), "output", SinkMode.REPLACE);
 
-       Pipe pipe = new Pipe("copy");
-
-       Flow flow = new HadoopFlowConnector().connect(src, snk, pipe);
-
-       flow.complete();
-
-   }
 
 
 }
