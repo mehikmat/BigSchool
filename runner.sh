80af9feb4c8e44d543d2c63cdaa3ae9bba05df33
hdhamee <hdhamee@deerwalk.com>
2023-02-02 14:04:30 +0545

    d

--- runner.sh
+++ runner.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
 #Run Job
-hadoop jar target/BigSchoolCascading-1.0.jar -D mapreduce.map.log.level=ERROR -D mapreduce.reduce.log.level=ERROR
+echo "mvn clean ............"
+mvn clean package -DskipTests
+echo "MK dirs ............"
+mkdir -p input/Eligibility
+mkdir -p input/Medical
+mkdir -p input/Pharmacy
+echo "run generate data ............"
+rm -rf input
+java -jar target/BigSchoolCascading-1.0.jar generate
+echo "Copy data to hdfs ............"
+hadoop fs -rmr input
+hadoop fs -rmr output
+hadoop fs -put input .
+echo "Running main job ............"
+hadoop jar target/BigSchoolCascading-1.0.jar merged
