d81878802664afbd0f400a6584e39a4f3cf8c69b
Hikmat Dhamee <me.hemant.available@gmail.com>
2025-09-04 11:30:04 +0545

    upgrade cascading

--- src/main/java/com/bigschool/Main.java
+++ src/main/java/com/bigschool/Main.java
@@ -5,23 +5,21 @@ import cascading.flow.hadoop2.Hadoop2MR1FlowConnector;
 import cascading.pipe.Pipe;
 import cascading.scheme.hadoop.TextDelimited;
 import cascading.tap.SinkMode;
-import cascading.tap.Tap;
 import cascading.tap.hadoop.Hfs;
 import cascading.tuple.Fields;
 
 public class Main {
 
     public static void main(String[] args) {
-        if(args.length < 3){
-            System.out.println("Insufficient arguments: jobName input output");
-            System.exit(1);
-        }
-        Tap src1 = new Hfs(new TextDelimited(new Fields("a","b","c"),";"), args[1], SinkMode.KEEP);
-        Tap snk1 = new Hfs(new TextDelimited(new Fields("a","b","c"),";"), args[2], SinkMode.REPLACE);
+        String inputFile = "input.csv";
+        String outputFile = "output.csv";
+        Hfs src1 = new Hfs(new TextDelimited(new Fields("a", "b", "c"), ";"), inputFile, SinkMode.KEEP);
+        Hfs snk1 = new Hfs(new TextDelimited(new Fields("a", "b", "c"), ";"), outputFile, SinkMode.REPLACE);
 
         Pipe pipe = new Pipe("test");
 
-        Flow flow = new Hadoop2MR1FlowConnector().connect(args[0],src1, snk1, pipe);
-        flow.complete();
+        Hadoop2MR1FlowConnector hadoop2MR1FlowConnector = new Hadoop2MR1FlowConnector();
+        Flow hadoopFlow = hadoop2MR1FlowConnector.connect(inputFile, src1, snk1, pipe);
+        hadoopFlow.complete();
     }
 }
